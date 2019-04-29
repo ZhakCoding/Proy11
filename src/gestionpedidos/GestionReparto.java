0f856beca57ccbdeb5331907ac235a6d9d667d6f
Sergio de Propios <infosergiodp@gmail.com>
2019-04-30 00:12:15 +0200

    Update GestionReparto.java

--- src/gestionpedidos/GestionReparto.java
+++ src/gestionpedidos/GestionReparto.java
@@ -9,7 +9,13 @@ public class GestionReparto {
 	private Mapa mapa;
 	
 	public GestionReparto(Mapa mapa){
-		//TO-DO
+		this.mapa = mapa;
+		this.gestoresLocales = new GestionRepartoLocal[4];
+
+		// Creamos los gestores locales
+		for(int i = 0; i < this.gestoresLocales.length; i++) {
+			this.gestoresLocales[i] = new GestionRepartoLocal();
+		}
 	}
 	
 	public Mapa getMapa() {
@@ -26,6 +32,12 @@ public class GestionReparto {
 				this.gestoresLocales[i].getCodEsperandoMoto().size() + ";" +
 				this.gestoresLocales[i].getCodEsperandoFurgo().size() ;
 	}
+
+	// Zona 0: X  [0, maxCoordX/2] Y  [0, maxCoordY/2]
+	// Zona 1: X  [0, maxCoordX/2] Y  [maxCoordY/2 + 1, maxCoordY]
+	// Zona 2: X  [maxCoordX/2 + 1, maxCoordX] Y  [0, maxCoordY/2]
+	// Zona 3: X  [maxCoordX/2 + 1, maxCoordX] Y  [maxCoordY/2 + 1, maxCoordY]
+	
 	
 	//PRE: el transporte no ha sido asignado a ninguna zona
 	public void addTransporteLocalidad(Transporte transporte) {
