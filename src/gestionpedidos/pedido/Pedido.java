89ae4552e572d1e074722b88114f20a214b45884
Sergio de Propios <infosergiodp@gmail.com>
2019-04-30 02:44:27 +0200

    Final

--- src/gestionpedidos/pedido/Pedido.java
+++ src/gestionpedidos/pedido/Pedido.java
@@ -13,52 +13,52 @@ import anotacion.Programacion2;
 )
 
 public class Pedido {
- private Cliente cliente;
- private PlatoComida[] comidas;
- private Restaurante restaurante;
- private double importe; 
- private Transporte transporte;
- private double peso;
- 
- public Pedido(Cliente cliente, PlatoComida[] comidas, Restaurante restaurante) {  
-  this.cliente = cliente;
-  this.comidas = comidas;
-  this.restaurante = restaurante;
+    private Cliente cliente;
+    private PlatoComida[] comidas;
+    private Restaurante restaurante;
+    private double importe; 
+    private Transporte transporte;
+    private double peso;
 
-  // Calculo del importe y peso
-  this.importe = 0;
-  this.peso = 0;
-  for(int i = 0; i < comidas.length; i++) {
-   this.importe += comidas[i].getPrecio();
-   this.peso += comidas[i].getPeso();
-  }
- }
- 
- public double coste(Transporte transporte) {
-  return importe + transporte.coste(restaurante.getCodigo()) + transporte.coste(restaurante.getCodigo(), cliente.getCodigo());
- }
- 
- public double getPeso() {
-  return peso;
- }
- 
- public double getImporte() {
-  return importe;
- } 
+    public Pedido(Cliente cliente, PlatoComida[] comidas, Restaurante restaurante) {  
+        this.cliente = cliente;
+        this.comidas = comidas;
+        this.restaurante = restaurante;
 
- public Transporte getTransporte() {
-  return transporte;
- }
+        // Calculo del importe y peso
+        this.importe = 0;
+        this.peso = 0;
+        for(int i = 0; i < comidas.length; i++) {
+            this.importe += comidas[i].getPrecio();
+            this.peso += comidas[i].getPeso();
+        }
+    }
 
- public void setTransporte(Transporte transporte) {
-  this.transporte = transporte;
- }
- 
- public Cliente getCliente() {
-  return cliente;
- }
- 
- public Restaurante getRestaurante() {
-  return restaurante;
- }
+    public double coste(Transporte transporte) {
+        return importe + transporte.coste(restaurante.getCodigo()) + transporte.coste(restaurante.getCodigo(), cliente.getCodigo());
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public double getImporte() {
+        return importe;
+    } 
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public Restaurante getRestaurante() {
+        return restaurante;
+    }
+
+    public Transporte getTransporte() {
+        return transporte;
+    }
+
+    public void setTransporte(Transporte transporte) {
+        this.transporte = transporte;
+    }
 }
--- src/gestionpedidos/transportes/FurgonetaPropia.java
+++ src/gestionpedidos/transportes/FurgonetaPropia.java
@@ -14,7 +14,7 @@ import anotacion.Programacion2;
 
 public class FurgonetaPropia extends Furgoneta {
 	private double velocidadMedia = 30;
-	private final double EUROS_P_HORA = 40;
+	private static final double EUROS_P_HORA = 40;
 		
 	public FurgonetaPropia(String codigo, Mapa mapa, double tara) {
 		super(codigo, mapa, tara);
